c7d415c408fbf03cb91b4547c6bb11ebfc566c2a
Jonathan Vega (TMNA) <jonathan.vega@toyota.com>
2019-08-15 15:59:26 -0400

    Fixed images not displaying.

--- src/components/DisplayEnemy.js
+++ src/components/DisplayEnemy.js
@@ -1,16 +1,18 @@
 import React from "react";
-
+import image from "../Images/Enemies/slime.jpg"
  export const DisplayEnemy = (props) => {
-     console.log(props.EnemyData.monsterHealth)
+      // console.log(props.EnemyData.monsterImage)
     return (
       <div>
         <img
           className="Image"
-          src={require(`../Images/Enemies/slime.jpg`)}
+          src={props.EnemyData.monsterImage}
           alt="slime"
         //   onClick={this.playerAttack}
         />
         <div>Enemy Health: {props.EnemyData.monsterHealth}</div>
+        <div>Enemy Level: {props.EnemyData.monsterLevel}</div>
+
       </div>
     );
   };
--- src/container/App.js
+++ src/container/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import enemies from "../JSON/Enemies.json";
 import player from "../JSON/Player.json";
 import { DisplayEnemy } from "../components/DisplayEnemy";
@@ -6,12 +6,16 @@ import "./App.css";
 
 
 
-class App extends Component {
-  state = {
-    playerData: player,
-    enemyData: enemies,
-    enemyDefeated: true,
-  };
+// class App extends Component {
+  const App = () => {
+  const [playerData, setPlayerData] = useState(player)
+  const [enemyData, setEnemyData] = useState(enemies)
+
+  // state = {
+  //   playerData: player,
+  //   enemyData: enemies,
+  //   enemyDefeated: true,
+  // };
 
   // componentDidMount() {
   //   this.updatePlayerInfo();
@@ -43,73 +47,73 @@ class App extends Component {
   //     levelExp: lExp
   //   });
   // };
-  displayPlayerinfo = () => {
-    let data = this.state.playerData.map((player, index) => {
-      return (
-        <div className="PlayerInfo">
-          Kills: {this.state.playerKills}, Level: {this.state.playerLevel}, Exp:{" "}
-          {this.state.currentExp} / {this.state.levelExp}
-        </div>
-      );
-    });
-    return data;
-  };
+  // displayPlayerinfo = () => {
+  //   let data = this.state.playerData.map((player, index) => {
+  //     return (
+  //       <div className="PlayerInfo">
+  //         Kills: {this.state.playerKills}, Level: {this.state.playerLevel}, Exp:{" "}
+  //         {this.state.currentExp} / {this.state.levelExp}
+  //       </div>
+  //     );
+  //   });
+  //   return data;
+  // };
 
-  enemySpawner = () => {
-    //Map data from state and assign it to variable.
-    let eInfo = this.state.enemyData.map((enemy, index) => {
-      return enemy;
-    });
+  // enemySpawner = () => {
+  //   //Map data from state and assign it to variable.
+  //   let eInfo = this.state.enemyData.map((enemy, index) => {
+  //     return enemy;
+  //   });
 
-    //Generate a random number
-    let enemyArr = Math.floor(Math.random() * eInfo.length + 1);
-    if (enemyArr == 0) {
-      enemyArr += 1;
-    }
+  //   //Generate a random number
+  //   let enemyArr = Math.floor(Math.random() * eInfo.length + 1);
+  //   if (enemyArr == 0) {
+  //     enemyArr += 1;
+  //   }
 
-    //Filter data based on monsterID's value which is enemyArr
-    let randEnemy = eInfo.filter(mob => {
-      // console.log(enemyArr);
-      return mob.monsterID == enemyArr;
-    });
+  //   //Filter data based on monsterID's value which is enemyArr
+  //   let randEnemy = eInfo.filter(mob => {
+  //     // console.log(enemyArr);
+  //     return mob.monsterID == enemyArr;
+  //   });
 
-    let enName = randEnemy.map(enemy => {
-      return enemy.monsterName;
-    });
-    let enLevel = randEnemy.map(enemy => {
-      return enemy.monsterLevel;
-    });
-    let enExp = randEnemy.map(enemy => {
-      return enemy.monsterExp;
-    });
-    let enHealth = randEnemy.map(enemy => {
-      return enemy.monsterHealth;
-    });
-    let enImage = randEnemy.map(enemy => {
-      return enemy.monsterImage;
-    });
+  //   let enName = randEnemy.map(enemy => {
+  //     return enemy.monsterName;
+  //   });
+  //   let enLevel = randEnemy.map(enemy => {
+  //     return enemy.monsterLevel;
+  //   });
+  //   let enExp = randEnemy.map(enemy => {
+  //     return enemy.monsterExp;
+  //   });
+  //   let enHealth = randEnemy.map(enemy => {
+  //     return enemy.monsterHealth;
+  //   });
+  //   let enImage = randEnemy.map(enemy => {
+  //     return enemy.monsterImage;
+  //   });
 
-    this.setState(
-      {
-        enemyHealth: enHealth,
-        enemyLevel: enLevel,
-        enemyExp: enExp,
-        enemyImage: enImage + ".jpg",
-        enemyName: enName,
-        enemyDefeated: false
-      },
-      console.log("Spawning a " + enName)
-    );
-  };
+  //   this.setState(
+  //     {
+  //       enemyHealth: enHealth,
+  //       enemyLevel: enLevel,
+  //       enemyExp: enExp,
+  //       enemyImage: enImage + ".jpg",
+  //       enemyName: enName,
+  //       enemyDefeated: false
+  //     },
+  //     console.log("Spawning a " + enName)
+  //   );
+  // };
 
-  playerAttack = () => {
-    let pDamage = this.state.playerData.map((player, index) => {
-      return player.playerDamage;
-    });
-    this.setState({
-      enemyHealth: this.state.enemyHealth - pDamage
-    });
-  };
+  // playerAttack = () => {
+  //   let pDamage = this.state.playerData.map((player, index) => {
+  //     return player.playerDamage;
+  //   });
+  //   this.setState({
+  //     enemyHealth: this.state.enemyHealth - pDamage
+  //   });
+  // };
   // displayEnemy = () => {
   //   return (
   //     <div>
@@ -127,68 +131,66 @@ class App extends Component {
   //   );
   // };
 
-  killCounter = () => {
-    let killNumber = this.state.playerData.map((player, index) => {
-      // console.log("counter: "+ player.killcount);
-      return player.killcount;
-    });
+  // killCounter = () => {
+  //   let killNumber = this.state.playerData.map((player, index) => {
+  //     // console.log("counter: "+ player.killcount);
+  //     return player.killcount;
+  //   });
 
-    this.setState({
-      // player.killcount: this.state.killcount + 1,
-      playerKills: this.state.playerKills + killNumber[0] + 1,
-      playerExp: this.state.playerExp + this.state.enemyExp
-    });
-  };
+  //   this.setState({
+  //     // player.killcount: this.state.killcount + 1,
+  //     playerKills: this.state.playerKills + killNumber[0] + 1,
+  //     playerExp: this.state.playerExp + this.state.enemyExp
+  //   });
+  // };
 
-  gainExp = () => {
-    if (this.state.playerExp >= this.state.levelExp) {
-      let expOver = 0;
+  // gainExp = () => {
+  //   if (this.state.playerExp >= this.state.levelExp) {
+  //     let expOver = 0;
 
-      if (this.state.playerExp > this.state.levelExp) {
-        expOver = this.state.playerExp - this.state.levelExp;
-      }
-      this.setState(
-        {
-          playerLevel: this.state.playerLevel + 1,
-          playerExp: 0 + expOver,
-          levelExp: this.state.levelExp * (this.state.playerLevel + 1)
-        },
-        () => {
-          console.log(
-            "LEVEL UP! Now level: " +
-              this.state.playerLevel +
-              " " +
-              this.state.levelExp
-          );
-        }
-      );
-    }
-  };
+  //     if (this.state.playerExp > this.state.levelExp) {
+  //       expOver = this.state.playerExp - this.state.levelExp;
+  //     }
+  //     this.setState(
+  //       {
+  //         playerLevel: this.state.playerLevel + 1,
+  //         playerExp: 0 + expOver,
+  //         levelExp: this.state.levelExp * (this.state.playerLevel + 1)
+  //       },
+  //       () => {
+  //         console.log(
+  //           "LEVEL UP! Now level: " +
+  //             this.state.playerLevel +
+  //             " " +
+  //             this.state.levelExp
+  //         );
+  //       }
+  //     );
+  //   }
+  // };
 
-  render() {
-    if (this.state.enemyHealth == 0) {
-      this.setState({
-        enemyDefeated: true
-      });
-      this.killCounter();
-      this.enemySpawner();
-    }
+  // render() {
+    // if (this.state.enemyHealth == 0) {
+    //   this.setState({
+    //     enemyDefeated: true
+    //   });
+    //   this.killCounter();
+    //   this.enemySpawner();
+    // }
+
+    const pushDataToComponents = data => {
+      let rNum = Math.round(Math.random() * (data.length - 1))
+      // console.log(rNum)
+      return  <DisplayEnemy EnemyData={data[rNum]}/>
+    };
 
     return (
       <div className="App">
         <div className="ClickUI">
-          {/* {this.MonsterDefeated()} */}
-          {/* {this.displayEnemy()} */}
-          <DisplayEnemy EnemyData={this.state.enemyData[0]}/>
-          {/* {console.log(this.enemyInfo().monsterName)} */}
-          {/* {this.displayPlayerinfo()} */}
-          {/* {this.enemyInfo()} */}
-
-          {/* <div className="UIHeader">CLICK THE MONSTER TO ATTACK!</div> */}
-          {/* {this.spawnMonster()} */}
+          {pushDataToComponents(enemyData)}
         </div>
       </div>
     );
-  }
+  // }
 }
 export default App;
