841791402dd8d5819566e930077296f0c1b63d28
Jonnyv212 <jonnyv212@hotmail.com>
2019-09-03 20:14:36 -0400

    Added combat.js

--- src/containers/Combat.js
+++ src/containers/Combat.js
@@ -1,43 +1,25 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { setCurrentEnemyHealth } from "../actions/enemyActions";
+import {setEnemyHP} from "../containers/EnemyData"
 
 export const Combat = enemyData => {
   const dispatch = useDispatch();
-  const combatEnabled = useSelector(state => state.enemyStates.combatEnabled);
+  // const combatEnabled = useSelector(state => state.enemyStates.combatEnabled);
   let eHealth = enemyData.monsterHealth;
   //   let pHealth = currentPlayerHealth;
-  dispatch(setCombat(true));
 
-  //   document.getElementById("fightBtn").disabled = true;
-  if (combatEnabled == true) {
     var enemyInterval = setInterval(() => {
+      
       if (eHealth > 0) {
-        eHealth -= playerAttack();
+        eHealth -= 1;
         dispatch(setCurrentEnemyHealth(eHealth));
-        // console.log("current monster health: " + eHealth);
+        // setEnemyHP(eHealth)Z
+        console.log("current monster health: " + eHealth);
       } else if (eHealth <= 0) {
         console.log("Enemy defeated!");
-        dispatch(setCombat(false));
-        gainExp(currentEnemy.monsterExp);
-        lootCheck();
         clearInterval(enemyInterval);
-        setNewEnemy(eData);
+        // setNewEnemy(eData);
       }
     }, 1300);
-
-    // var playerInterval = setInterval(() => {
-    //   if (pHealth > 0) {
-    //     pHealth -= playerAttack();
-    //     dispatch(setCurrentEnemyHealth(pHealth));
-    //     // console.log("current monster health: " + eHealth);
-    //   } else if (pHealth <= 0) {
-    //     console.log("Enemy defeated!");
-    //     dispatch(setCombat(false));
-    //     // gainExp(currentEnemy.monsterExp)
-    //     // lootCheck();
-    //     clearInterval(playerInterval);
-    //     // setNewEnemy(eData);
-    //   }
-    // }, 1300);
-  }
 };
